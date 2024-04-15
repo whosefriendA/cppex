5713d3c220788b0c202853b06639056cc3f240e1
whosefriendA <2653344989@qq.com>
2024-04-15 20:51:57 +0800

    new]

--- no6.cc
+++ no6.cc
@@ -4,5 +4,16 @@
 #include<vector>
 using std::cout;
 using std::cin;
+using std::endl;
 using std::vector;
 using std::string;
+// void change(int);
+// int main(){
+//     int a=3;
+//     change(a);
+//     cout<<a<<endl;
+//     return 0;
+// }
+// void change(int a){
+//     a=5;
+// }
