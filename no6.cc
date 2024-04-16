caa9e5893378f3e9eaace4459696972ce382c0e3
whosefriendA <2653344989@qq.com>
2024-04-16 20:00:47 +0800

    new]

--- no6.cc
+++ no6.cc
@@ -17,3 +17,19 @@ using std::string;
 // void change(int a){
 //     a=5;
 // }
+//交换两个指针所指的值
+// void exchange(int* &ya,int* &yb){
+// int* temp=ya;
+// ya=yb;
+// yb=temp;
+// }
+// int main(){
+//     int a=3,b=5;
+//     int*pa=&a;
+//     int*pb=&b;
+//     int* &ya=pa;
+//     int* &yb=pb;
+//     exchange(ya,yb);
+//     cout<<*pa<<endl<<*pb<<endl;
+//     return 0;
+// }
