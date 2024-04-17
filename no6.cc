2257e2c6fe157b39dc6408724cea706ce9d8dacc
whosefriendA <2653344989@qq.com>
2024-04-17 19:25:55 +0800

    new]

--- no6.cc
+++ no6.cc
@@ -2,6 +2,7 @@
 #include<cstring>
 #include<cctype>
 #include<vector>
+#include<cstdlib>
 using std::cout;
 using std::cin;
 using std::endl;
@@ -18,18 +19,32 @@ using std::string;
 //     a=5;
 // }
 //交换两个指针所指的值
-// void exchange(int* &ya,int* &yb){
-// int* temp=ya;
-// ya=yb;
-// yb=temp;
+// void exchange(int* &pa,int* &pb){
+// int* temp=pa;
+// pa=pb;
+// pb=temp;
 // }
 // int main(){
 //     int a=3,b=5;
 //     int*pa=&a;
 //     int*pb=&b;
-//     int* &ya=pa;
-//     int* &yb=pb;
-//     exchange(ya,yb);
+//     exchange(pa,pb);
 //     cout<<*pa<<endl<<*pb<<endl;
 //     return 0;
 // }
+//交换两个值
+// void change(int &a,int &b){
+// int temp=a;
+// a=b;
+// b=temp;
+// }
+// int main(){
+//     int a=1;
+//     int b=10;
+//     change(a,b);
+//     cout<<a<<endl<<b;
+// }
+//使用尾置返回类型声明一个返回数组指针的函数
+// auto doit(int i)->int(*)[10];
+//使用decltype声明一个返回数组指针的函数
+//...
