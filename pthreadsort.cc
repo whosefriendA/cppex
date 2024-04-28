0e45cfd7593c08102fd4b8bd2838d2a532771cbf
whosefriendA <2653344989@qq.com>
2024-04-28 21:52:10 +0800

    new]

--- pthreadsort.cc
+++ pthreadsort.cc
@@ -2,14 +2,34 @@
 #include<vetor>
 #include<thread>
 #include<random>
+#include<mutex>
+
 void randomdata(std::vector<int>&data,int datasize,int maxnum,int minnum){
-std::random_device rd;//随机种子
-std::mt19937 gen(rd());//随机数引擎
-//分布函数
-std::uniform_int_distribution dis(minnum,maxnum);
-for(int i=0;i<datasize;i++){
-    data.push_back(dis(gen))
+std::srand(std::time(nullptr)); // 设置随机数种子为当前时间，以便每次生成的随机数序列不同
+    for (int i = 0; i < size; ++i) {
+        arr.push_back(std::rand() % 1000); 
+    }
 }
+
+void quickSort(std::vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; ++j) {
+            if (arr[j] < pivot) {
+                ++i;
+                std::swap(arr[i], arr[j]);
+            }
+        }
+        std::swap(arr[i + 1], arr[right]);
+        int pivotIndex = i + 1;
+
+        std::thread tLeft(quickSort, std::ref(arr), left, pivotIndex - 1);
+        std::thread tRight(quickSort, std::ref(arr), pivotIndex + 1, right);
+
+        tLeft.join();
+        tRight.join();
+    }
 }
 
 int main(){
@@ -17,5 +37,24 @@ int main(){
     int maxnum=100000;
     int minnum=0;
     std::vector<int> data;
+
+    randomdata(data,datasize,maxnum,minnum);
+    int numthread=std::thread::hardware_concurrency();
+    int arraysize=datasize/numthread;
+    std::vector<std::vector<int>>mularray(numthread);
+    for(int i=0;i<numthread;i++){
+        auto start=data.begin()+numsthread;
+        auto end=(i==numthread-1)?data.end():data.begin()+numthread;
+        mularray[i].assign(start,end);
+    }
+
+    std::vector<std::thread> threads;
+    for(int i=0;i<numthread;i++){
+        threads.emplace(quicksort,std::ref(mularray[i]),0,mularray.size()-1);
+    }
+    for(auto &t :threads){
+        t.join();
+    }
+
     
 }
