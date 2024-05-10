c53355bec77bce956ed93a27ddb7c1b11077bdad
whosefriendA <2653344989@qq.com>
2024-05-10 16:04:17 +0800

    new]

--- phi_dinner.cc
+++ phi_dinner.cc
@@ -6,23 +6,20 @@
 #define phi_num 5
 class Phi{
     public:
-    eat(){
-        std::thread phieat([this]{std::cout<<this<<"吃得饱饱，放下了他的筷子";})
+    void eat(){
+        std::thread phieat([this]{std::cout<<this<<"吃得饱饱，放下了他的筷子"<<std::endl;});
+        phieat.join();
     }
-    ~Phi(){
-        phieat,join();
-    }
-}
+};
 //方案一  使用互斥锁（可能死锁）
 int main(){
-    std::vector<class Phi>(5);
+    std::vector<class Phi> Philo(5);
     std::mutex left_chop;
     std::mutex right_chop;
-    std::unique_lock<mutex> lock(left_chop);
-    std::unique_lock<mutex> lock(right_chop);
     for(int i=0;i<phi_num;i++){
-        std::unique_lock<mutex> lock(left_chop);
-        std::unique_lock<mutex> lock(right_chop);
-        
+        std::unique_lock<std::mutex> lock1(left_chop);
+        std::unique_lock<std::mutex> lock2(right_chop);
+        Philo[i].eat();
     }
 }
+//方案二 使用信号量
