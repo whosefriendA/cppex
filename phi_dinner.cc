1cd2a50b858debb472a6f7d43f08cd9e31ebbba8
whosefriendA <2653344989@qq.com>
2024-05-12 21:32:55 +0800

    new]

--- phi_dinner.cc
+++ phi_dinner.cc
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<semaphore.h>
 #include<thread>
 #include<mutex>
 #include<vector>
@@ -7,19 +8,31 @@
 class Phi{
     public:
     void eat(){
-        std::thread phieat([this]{std::cout<<this<<"吃得饱饱，放下了他的筷子"<<std::endl;});
+        std::thread phieat([this]{std::cout<<this<<"吃完饭，放下了他的筷子"<<std::endl;});
         phieat.join();
     }
 };
-//方案一  使用互斥锁（可能死锁）
+//方案一  使用互斥锁
+// int main(){
+//     std::vector<class Phi> Philo(5);
+//     std::mutex left_chop;
+//     std::mutex right_chop;
+//     for(int i=0;i<phi_num;i++){
+//         std::unique_lock<std::mutex> lock1(left_chop);
+//         std::unique_lock<std::mutex> lock2(right_chop);
+//         Philo[i].eat();
+//     }
+// }
+//方案二 使用信号量(这里只能使用c语言的信号量，c++标准库中没有对信号量的直接定义)
 int main(){
+    sem_t chop(5);
     std::vector<class Phi> Philo(5);
-    std::mutex left_chop;
-    std::mutex right_chop;
     for(int i=0;i<phi_num;i++){
-        std::unique_lock<std::mutex> lock1(left_chop);
-        std::unique_lock<std::mutex> lock2(right_chop);
+        sem_wait(&chop[i]);
+        sem_wait(&chop[i+1]);
         Philo[i].eat();
+        sem_post(&chop[i]);
+        sem_post(&chop[i+1]);
     }
+
 }
-//方案二 使用信号量
