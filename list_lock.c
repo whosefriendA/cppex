67f779464f0db25bad6f50008934d65ecda13141
whosefriendA <2653344989@qq.com>
2024-05-16 20:20:37 +0800

    new]

--- list_lock.c
+++ list_lock.c
@@ -4,11 +4,21 @@
 #include <stdlib.h>
 
 void listInit(list_lock_t* list) {
-   perror("This function is not implemented");
+   list->head=NULL;
+   pthread_mutex_init(&list->mutex,NULL);
+   pthread_cond_init(&list->cond,NULL);
 }
 
 void producer(list_lock_t* list, DataType value) {
-  perror("This function is not implemented");
+  Lnode* buf=malloc(Lnode*)(sizeof(Lnode));
+  buf.next=NULL;
+  buf.value=value
+  
+  pthread_mutex_lock(&list->mutex);
+  buf.next=list.head;
+  list.head=buf;
+  pthread_cond_signal(&list->cond);
+  pthread_mutex_unlock(&list->mutex);
 }
 
 void consumer(list_lock_t* list) {
