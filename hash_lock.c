b872a1d6e7fb53447f4becdfc16c2b8c573da89d
whosefriendA <2653344989@qq.com>
2024-05-23 20:25:32 +0800

    new]

--- hash_lock.c
+++ hash_lock.c
@@ -4,99 +4,96 @@
 #include <stdlib.h>
 
 void hashInit(hash_lock_t* bucket) {
-  for(int i=0;i<HASHNUM;i++){
-    bucket->table[i].head=NULL;
-    pthread_mutex_init(&bucket->table[i].mutex,NULL);
-  }
+    for(int i=0;i<HASHNUM;i++)
+    {
+        bucket->table[i].head=NULL;
+        pthread_mutex_init(&(bucket->table[i].mutex),NULL);
+    }
 }
 
 int getValue(hash_lock_t* bucket, int key) {
-    int hashIndex = HASH(key);
-    pthread_mutex_lock(&bucket->table[hashIndex].mutex);
-
-    Hlist node = bucket->table[hashIndex].head;
-    while (node != NULL) {
-        if (node->key == key) {
-            int value = node->value;
-            pthread_mutex_unlock(&bucket->table[hashIndex].mutex);
-            return value;
+    int n=HASH(key);
+    pthread_mutex_lock(&bucket->table[n].mutex); 
+    Hlist p=bucket->table[n].head;
+    while(p)
+    {
+        if(p->key==key)
+        {
+            pthread_mutex_unlock(&bucket->table[n].mutex); 
+            return p->value;
         }
-        node = node->next;
+        p=p->next;
     }
-    
-    pthread_mutex_unlock(&bucket->table[hashIndex].mutex);
-    return -1; 
+    pthread_mutex_unlock(&bucket->table[n].mutex); 
+    return -1;
 }
 
-
 void insert(hash_lock_t* bucket, int key, int value) {
-    Hlist newnode = (Hlist)malloc(sizeof(Hnode));
-    newnode->key = key;
-    newnode->value = value;
-    newnode->next = NULL;
-
-    int hashIndex = HASH(key);
-    pthread_mutex_lock(&bucket->table[hashIndex].mutex);
-
-    Hlist cur = bucket->table[hashIndex].head;
-    if (cur == NULL) {
-        bucket->table[hashIndex].head = newnode;
-    } else {
-      while (cur->next != NULL) {
-        if (cur->key == key) {
-          cur->value = value; 
-          pthread_mutex_unlock(&bucket->table[hashIndex].mutex);
-          free(newnode); 
-          return;
+    int n=HASH(key);
+    pthread_mutex_lock(&bucket->table[n].mutex); 
+    Hlist p=bucket->table[n].head;
+    while(p)
+    {
+        if(p->key==key)
+        {
+            p->value=value;
+            pthread_mutex_unlock(&bucket->table[n].mutex); 
+            return;
         }
-            cur = cur->next;
-        }
-        cur->next = newnode;
+        p=p->next;
     }
-
-    pthread_mutex_unlock(&bucket->table[hashIndex].mutex);
+    Hnode *node=(Hnode *)malloc(sizeof(Hnode));
+    node->key=key;
+    node->value=value;
+    node->next=bucket->table[n].head;
+    bucket->table[n].head=node;
+    pthread_mutex_unlock(&bucket->table[n].mutex); 
 }
 
 int setKey(hash_lock_t* bucket, int key, int new_key) {
-    int oldHashIndex = HASH(key);
-    int newHashIndex = HASH(new_key);
-
-    if (oldHashIndex == newHashIndex) {
-        pthread_mutex_lock(&bucket->table[oldHashIndex].mutex);
+    int n1=HASH(key);
+    int n2=HASH(new_key);
+    if (n1 == n2) {
+        pthread_mutex_lock(&bucket->table[n1].mutex);
     } else {
-        if (oldHashIndex < newHashIndex) {
-            pthread_mutex_lock(&bucket->table[oldHashIndex].mutex);
-            pthread_mutex_lock(&bucket->table[newHashIndex].mutex);
+        if (n1 < n2) {
+            pthread_mutex_lock(&bucket->table[n1].mutex);
+            pthread_mutex_lock(&bucket->table[n2].mutex);
         } else {
-            pthread_mutex_lock(&bucket->table[newHashIndex].mutex);
-            pthread_mutex_lock(&bucket->table[oldHashIndex].mutex);
+            pthread_mutex_lock(&bucket->table[n2].mutex);
+            pthread_mutex_lock(&bucket->table[n1].mutex);
         }
     }
-    Hlist prev = NULL;
-    Hlist node = bucket->table[oldHashIndex].head;
-    while (node != NULL) {
-        if (node->key == key) {
-          if (prev == NULL) {
-            bucket->table[oldHashIndex].head = node->next;
-          } else {
-            prev->next = node->next;
-          }
-          node->key = new_key;
-          node->next = bucket->table[newHashIndex].head;
-          bucket->table[newHashIndex].head = node;
-          if (oldHashIndex != newHashIndex) {
-             pthread_mutex_unlock(&bucket->table[newHashIndex].mutex);
-          }
-          pthread_mutex_unlock(&bucket->table[oldHashIndex].mutex);
-          return 0;
+    Hlist prev=NULL;
+    Hlist p=bucket->table[n1].head;
+    while(p)
+    {
+    if(p->key==key)
+     {
+         if(prev)
+         {
+            prev->next=p->next;
+         }
+        else
+         {
+            bucket->table[n1].head=p->next;
         }
-        prev = node;
-        node = node->next;
+            p->key=new_key;
+            p->next=bucket->table[n2].head;
+            bucket->table[n2].head=p;
+         if (n1 != n2) {
+             pthread_mutex_unlock(&bucket->table[n2].mutex);
+          }
+          pthread_mutex_unlock(&bucket->table[n1].mutex);
+        return 0;
+        }
+        prev=p;
+        p=p->next;
     }
-    if (oldHashIndex != newHashIndex) {
-        pthread_mutex_unlock(&bucket->table[newHashIndex].mutex);
+    if (n1 != n2) {
+        pthread_mutex_unlock(&bucket->table[n2].mutex);
     }
-    pthread_mutex_unlock(&bucket->table[oldHashIndex].mutex);
-    return -1; 
+    pthread_mutex_unlock(&bucket->table[n1].mutex);
+    return -1;
 }
 
